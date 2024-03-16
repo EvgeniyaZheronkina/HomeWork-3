7955d2f0d18899583eb3172d08c4cb5d91c06c38
Евгения <zheronkina86@gmail.com>
2024-03-16 21:49:04 +0300

    Сделаны 1 и 2 задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        System.out.println("Задание 1");
+        int age = 16;
+
+        if (age >= 18){
+            System.out.println("Если возраст человека равен" +age +", то он совершенолетний");
+        }
+        else{
+            System.out.println("Если возраст человека равен " +age +", то он не достиг совершенолетия, нужно немного подождать");
+        }
+
+        //Задание 2
+        System.out.println("Задание 2");
+        int temperature = 7;
+
+        if (temperature <= 5) {
+            System.out.println("На улице " +temperature+ " градусов. На улице холодно, нужно надеть шапку");
+        }
+        else {
+            System.out.println("На улице " +temperature+ " градусов. Сегодня тепло, можно идти без шапки");
+        }
     }
 }
